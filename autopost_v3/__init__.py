a314fd344189bab5e281918e7256d42eb4a87b43
Mark Heath <MHLoppy@users.noreply.github.com>
2022-01-31 22:26:18 +1100

    Add files via upload

---
+++ autopost_v3/__init__.py
@@ -0,0 +1,12 @@
+import json
+from pathlib import Path
+
+from .autopost_v3 import Autopost
+
+with open(Path(__file__).parent / "info.json") as fp:
+    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
+
+
+def setup(bot):
+    n = Autopost(bot)
+    bot.add_cog(n)
